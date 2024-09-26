204639b2a0a222fc50bb7cba62dc5aa5babcf5e0
MERVE EKŞİ <merveeksii61@gmail.com>
2024-09-26 19:28:18 +0300

    .
    .

---
+++ rentACar/RentACar/Persistence/Context/BaseDbContext.cs
@@ -0,0 +1,33 @@
+using System.Reflection;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace Persistence.Context;
+
+public sealed class BaseDbContext:DbContext
+{
+    private IConfiguration Configuration { get; set; }
+    public DbSet<Brand> Brands { get; set; }
+    // public DbSet<Car> Cars { get; set; }
+    // public DbSet<Fuel> Fuels { get; set; }
+    // public DbSet<Transmission> Transmissions { get; set; }
+    // public DbSet<Model> Models { get; set; }
+    // public DbSet<OperationClaim> OperationClaims { get; set; }
+    // public DbSet<OtpAuthenticator> OtpAuthenticators { get; set; }
+    // public DbSet<RefreshToken> RefreshTokens { get; set; }
+    // public DbSet<User> Users { get; set; }
+    // public DbSet<UserOperationClaim> UserOperationClaims { get; set; }
+    // public DbSet<EmailAuthenticator> EmailAuthenticators { get; set; }
+
+    public BaseDbContext(DbContextOptions dbContextOptions, IConfiguration configuration) : base(dbContextOptions)
+    {
+        Configuration = configuration;
+        Database.EnsureCreated();
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
+}
---
+++ rentACar/RentACar/Persistence/PersistenceServiceRegistration.cs
@@ -0,0 +1,30 @@
+using Application.Services.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Persistence.Context;
+using Persistence.Repositories;
+
+namespace Persistence;
+
+public static class PersistenceServiceRegistration
+{
+    public static IServiceCollection AddPersistenceServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        //services.AddDbContext<BaseDbContext>(options => options.UseInMemoryDatabase("nArchitecture"));
+
+        services.AddDbContext<BaseDbContext>(options => options.UseSqlServer(configuration.GetConnectionString("RentACar")));
+
+        services.AddScoped<IBrandRepository, BrandRepository>();
+        // services.AddScoped<IModelRepository, ModelRepository>();
+        //
+        // services.AddScoped<IEmailAuthenticatorRepository, EmailAuthenticatorRepository>();
+        // services.AddScoped<IOperationClaimRepository, OperationClaimRepository>();
+        // services.AddScoped<IOtpAuthenticatorRepository, OtpAuthenticatorRepository>();
+        // services.AddScoped<IRefreshTokenRepository, RefreshTokenRepository>();
+        // services.AddScoped<IUserRepository, UserRepository>();
+        // services.AddScoped<IUserOperationClaimRepository, UserOperationClaimRepository>();
+
+        return services;
+    }
+}
