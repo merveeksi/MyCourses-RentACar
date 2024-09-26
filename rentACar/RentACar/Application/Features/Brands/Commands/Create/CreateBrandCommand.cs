6648dd0532643d3c4d2dc5326e97b11d89b6724d
MERVE EKŞİ <merveeksii61@gmail.com>
2024-09-26 19:25:07 +0300

    .
    .

---
+++ rentACar/RentACar/Application/Features/Brands/Commands/Create/CreateBrandCommand.cs
@@ -0,0 +1,50 @@
+using Application.Services.Repositories;
+using AutoMapper;
+using Domain.Entities;
+using MediatR;
+
+namespace Application.Features.Brands.Commands.Create;
+
+public class
+    CreateBrandCommand : IRequest<CreatedBrandResponse> //,ITransactionalRequest,ICacheRemoverRequest,ILoggableRequest
+{
+    public string Name { get; set; }
+
+    // public string CacheKey => "";
+    //
+    // public bool BypassCache => false;
+    //
+    // public string? CacheGroupKey => "GetBrands";
+    //
+    public class CreateBrandCommandHandler : IRequestHandler<CreateBrandCommand, CreatedBrandResponse>
+    {
+        private readonly IBrandRepository _brandRepository;
+        private readonly IMapper _mapper;
+        //private readonly BrandBusinessRules _brandBusinessRules;
+
+        public CreateBrandCommandHandler(IBrandRepository brandRepository, IMapper mapper)
+            //BrandBusinessRules brandBusinessRules)
+        {
+            _brandRepository = brandRepository;
+            _mapper = mapper;
+            //_brandBusinessRules = brandBusinessRules;
+        }
+
+        public async Task<CreatedBrandResponse>? Handle(CreateBrandCommand request, CancellationToken cancellationToken)
+        {
+            //await _brandBusinessRules.BrandNameCannotBeDuplicatedWhenInserted(request.Name);
+
+            Brand brand = _mapper.Map<Brand>(request);
+            brand.Id = Guid.NewGuid();
+
+            //Brand brand2 = _mapper.Map<Brand>(request);
+            //brand2.Id = Guid.NewGuid();
+
+            await _brandRepository.AddAsync(brand);
+            //await _brandRepository.AddAsync(brand2);
+
+            CreatedBrandResponse createdBrandResponse = _mapper.Map<CreatedBrandResponse>(brand);
+            return createdBrandResponse;
+        }
+    }
+}
---
+++ rentACar/RentACar/Application/Features/Brands/Queries/GetList/GetListBrandQuery.cs
@@ -0,0 +1,48 @@
+using Application.Services.Repositories;
+using AutoMapper;
+using Core.Application.Request;
+using Core.Application.Responses;
+using Core.Persistence.Paging;
+using Domain.Entities;
+using MediatR;
+
+namespace Application.Features.Brands.Queries.GetList;
+
+//kullanıcının bize gönderdiği sayfa bilgileri
+public class GetListBrandQuery : IRequest<GetListResponse<GetListBrandListItemDto>>//, ICachableRequest, ILoggableRequest
+{
+    public PageRequest PageRequest { get; set; } //kullanıcının bize gönderdiği sayfa bilgileri
+
+    // public string CacheKey => $"GetListBrandQuery({PageRequest.PageIndex},{PageRequest.PageSize})";
+    //
+    // public bool BypassCache {get;}
+    // public TimeSpan? SlidingExpiration { get; }
+    //
+    // public string? CacheGroupKey => "GetBrands";
+    //
+    public class GetListBrandQueryHandler : IRequestHandler<GetListBrandQuery, GetListResponse<GetListBrandListItemDto>>
+    {
+        private readonly IBrandRepository _brandRepository;
+        private readonly IMapper _mapper;
+    
+        public GetListBrandQueryHandler(IBrandRepository brandRepository, IMapper mapper)
+        {
+            _brandRepository = brandRepository;
+            _mapper = mapper;
+        }
+    
+        public async Task<GetListResponse<GetListBrandListItemDto>> Handle(GetListBrandQuery request, CancellationToken cancellationToken)
+        {
+            Paginate<Brand> brands =  await _brandRepository.GetListAsync(
+                index: request.PageRequest.PageIndex,
+                size: request.PageRequest.PageSize,
+                cancellationToken : cancellationToken,
+                withDeleted:true
+            );
+    
+            GetListResponse<GetListBrandListItemDto> response = _mapper.Map<GetListResponse<GetListBrandListItemDto>>(brands);
+            return response;
+    
+        }
+    }
+}
