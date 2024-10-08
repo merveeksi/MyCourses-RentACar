726e6cb99a2f0201ce6010b31e12347f744a9cb3
MERVE EKŞİ <merveeksii61@gmail.com>
2024-10-08 11:47:16 +0300

    Merge conflict çözüldü

--- rentACar/RentACar/Domain/Entities/Brand.cs
+++ rentACar/RentACar/Domain/Entities/Brand.cs
@@ -1,4 +1,9 @@
 using Core.Persistence.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Domain.Entities;
 
@@ -6,25 +11,14 @@ public class Brand:Entity<Guid>
 {
     public string Name { get; set; }
 
-<<<<<<< HEAD
-    // public virtual ICollection<Model> Models { get; set; }
-    //
+    public virtual ICollection<Model> Models { get; set; }
+
     public Brand()
     {
-        //Models = new HashSet<Model>();
-    }
-    
-    public Brand(Guid id, string name)//:this()
-=======
-    public virtual ICollection<Model> Models { get; set; }
-    
-    public Brand()
-    { 
         Models = new HashSet<Model>();
     }
-    
-    public Brand(Guid id, string name):this() //this ile birlikte çalıştırılan constructor, üstteki constructor'ı çalıştırır
->>>>>>> f8079a9 (Initial commit for MyCourses_RentACar)
+
+    public Brand(Guid id, string name):this()
     {
         Id = id;
         Name = name;
